2f4df49d4b1c996afc3b727216288e78a9ae023e
Ineidy <160775201+Ineidy@users.noreply.github.com>
2024-03-01 20:11:41 +0100

    Promedio de cuatro numeros

--- RadioyPerimetro.py
+++
@@ -1,6 +0,0 @@
-
-radio = float(input("INGRESE EL RADIO DE SU CIRCULO: "))
-pi = float(3.1416)
-area = pi * (radio**2)
-perimetro = 2 * pi * radio
-print(f"El circulo con radio de {radio} tiene un area de {area} y un perimetro de {perimetro}")
---
+++ promedio.py
@@ -0,0 +1,11 @@
+print("VAMOS A CALCULAR EL PROMEDIO DE 4 NUMEROS!!")
+cantidad_num = 4
+prim = float(input("Digita el primer numero: "))
+segu = float(input("Digita el Segundo numero: "))
+terc = float(input("Digita el Tercer numero: "))
+cuar = float(input("Digita el Cuarto numero: "))
+suma = prim + segu + terc + cuar
+promedio = suma / cantidad_num
+
+print(f"La suma de estos cuarto numeros es : {suma},  y el promedio es: {promedio}")
+
