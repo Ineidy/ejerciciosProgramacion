350d8045b6f46c29a8dd33bbe6a875010dc49ce8
Ineidy <160775201+Ineidy@users.noreply.github.com>
2024-03-04 22:45:45 +0100

    Sacar la hipotenusa

--- numeroinvertido.py
+++
@@ -1,8 +0,0 @@
-numero = input("Escribe un numero de tres digitos para invertir: ")
-if  len(numero) != 3:
-     print("Error, solo tres digitos")
-     exit()
-else: 
-    numero_inv = str(numero)[::-1]
-    print()
-    print(f"El numero {numero}, invertido es {numero_inv}")
