c0e6c2ab182396008e2cbcdb4255301d06d8923e
Ineidy <160775201+Ineidy@users.noreply.github.com>
2024-03-22 07:03:28 -0500

    Determinar si un numero es par o inpar

---
+++ parOinpar.py
@@ -0,0 +1,8 @@
+
+while True: 
+        numero = int(input("Ingrese un numero: "))
+        defnumero = (numero%2)
+        if(defnumero==0):
+            print("Su numero es par")
+        elif(defnumero!=0):
+            print("Su numero es inpar")
