565e072653d4abadb16050a5718ce00e1a6d53ec
Ineidy <160775201+Ineidy@users.noreply.github.com>
2024-03-22 08:00:51 -0500

    Division

--- añosBisiestos.py
+++
@@ -1,9 +0,0 @@
-while True:
-
-    año = int(input("Ingrese un año: "))
-    añobi = año%4==0 and (año != 0 or año%400==0) 
-
-    if añobi and añobi:
-        print(f'{año} es bisiesto')
-    else:
-        print(f'{año} no es bisiesto')
