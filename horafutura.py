9ddd5b66aed6d3b407d2098b9941ed1637a3e085
Ineidy <160775201+Ineidy@users.noreply.github.com>
2024-03-22 07:04:35 -0500

    Determinar si un numero es par o inpar

--- horafutura.py
+++
@@ -1,9 +0,0 @@
-print("QUIERES SABER QUE HORA SERÁ EN X TIEMPO?")
-t = int(input("Escribe que hora es: "))
-print("")
-h = int(input("Escribe las horas que quieres avanzar: "))
-print("")
-htot = (t + h) % 24
-
-print(f"La hora despues de {h} horas es: {htot}")
-
--- parteDecimal.py
+++
@@ -1,3 +0,0 @@
-num = float(input("Ingrese un número: "))
-decimal = num - int(num)
-print(f"El decimal de {num} es {decimal}")
